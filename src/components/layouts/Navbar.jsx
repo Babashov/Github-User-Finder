d0df98ec22b4bf039dfa75402c6541fe7170e9ff
Babashov Mahammad <babashov.info@gmail.com>
2023-01-26 17:36:11 +0400

    added Page to the navigation links

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -16,10 +16,10 @@ function Navbar({title}) {
             <div className="flex-1 px-2 mx-2">
                 <div className="flex justify-end">
                     <Link to='/' className='btn btn-ghost btn-sm rounded-btn'>
-                        Home
+                        Home Page
                     </Link>
                     <Link to='/about' className='btn btn-ghost btn-sm rounded-btn'>
-                        About
+                        About Page
                     </Link>
                 </div>
             </div>
